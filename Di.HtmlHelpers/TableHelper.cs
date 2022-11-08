1d31c701b0a581e9d3798c4e3a13d69f7b97549c
Дмитрий Иглаков <cjdmitri@gmail.com>
2022-11-08 10:55:05 +0700

    1

--- Di.HtmlHelpers/TableHelper.cs
+++ Di.HtmlHelpers/TableHelper.cs
@@ -5,11 +5,17 @@ using System.ComponentModel;
 using System.Net.NetworkInformation;
 using System.Reflection;
 using Di.HtmlHelpers.Extensions;
+using System.Text.Encodings.Web;
+using System.Web;
+using System.Diagnostics;
 
 namespace Di.HtmlHelpers
 {
      public static class TableHelper
      {
+          private static Stopwatch sw = new Stopwatch();
+          private static PropertyInfo[] propertyInfos;
+
           /// <summary>
           /// Создание гибкой, настраиваемой таблицы
           /// </summary>
@@ -22,18 +28,21 @@ namespace Di.HtmlHelpers
           /// <param name="showSearch">Показывать строку поиска</param>
           /// <param name="replaceStrings">Какие строковые значения в ячейках данных следует заменить</param>
           /// <param name="excludeFields">Какие свойства модели следует исключить</param>
+          /// <param name="actions">Возможные дополнительные действия со строками. При наличии, показывается в первой колонке dropdown. Где Key - URL, value - текст ссылки</param>
           /// <returns></returns>
-          public static HtmlString CreateTable<T>(this IHtmlHelper html, 
+          public static HtmlString CreateTable<T>(this IHtmlHelper html,
              List<T> listItems,
              string? tableClass = null,
-             string? idBody = null,
+             string idBody = "tableBody",
              bool addHeader = true,
              bool showSearch = true,
              Dictionary<string, string>? replaceStrings = null,
-             string[]? excludeFields = null)
+             string[]? excludeFields = null,
+             Dictionary<string, string>? actions = null)
           {
+               sw.Start();
                //Получаем все свойства модели
-               PropertyInfo[] propertyInfos = Common.GetProperties<T>(excludeFields);
+               propertyInfos = Common.GetProperties<T>(excludeFields);
 
                //Устанавливаем класс для таблицы
                string cls = "";
@@ -43,10 +52,45 @@ namespace Di.HtmlHelpers
                //Контейнер для таблицы
                string result = $"<div style=\"overflow-x:auto\"><table {cls}>";
 
+               //Генерация заголовка и строки поиска
+               result += GenerateHeader(addHeader, showSearch, actions);
+
+               //Формируем тело таблицы
+               result += $"<tbody id=\"{idBody}\">";
+               foreach (var item in listItems)
+               {
+                    //Всплывающее меню дополнительных действий
+                    result += GenerateDropDownMenuItem(item, actions);
+                    result += GenerateRow(item, replaceStrings);
+               }
+               result += "</tbody></table></div>";
+
+               sw.Stop();
+               Console.WriteLine($"Генерация таблицы завершена: {sw.ElapsedMilliseconds} msec.");
+               return new HtmlString(result);
+          }
+
+          /// <summary>
+          /// Генерация заголовка таблицы и при необходимости строки поиска для колонок
+          /// </summary>
+          /// <param name="addHeader"></param>
+          /// <param name="showSearch"></param>
+          /// <param name="actions"></param>
+          /// <returns></returns>
+          private static string GenerateHeader(
+               bool addHeader = true,
+             bool showSearch = true,
+             Dictionary<string, string>? actions = null)
+          {
+               string result = "";
                //Устанавливаем заголовки для колонок
                if (addHeader)
                {
-                    result += "<thead><tr>";
+                    if (actions == null)
+                         result += "<thead><tr>";
+                    else
+                         result += "<thead><tr><th></th>";
+
                     foreach (var propertyInfo in propertyInfos)
                          result += $@"<th>{propertyInfo.GetDisplayName()}</th>";
                     result += "</tr>";
@@ -55,47 +99,94 @@ namespace Di.HtmlHelpers
                     if (showSearch)
                     {
                          result += "<tr>";
-                         foreach (var propertyInfo in propertyInfos)
-                              result += $"<th><input type=\"text\" class=\"form-control form-control-sm\"></th>";
+                         if (actions != null)
+                              result += $"<th></th>";
+                         for (int i = 0; i < propertyInfos.Length; i++)
+                         {
+                              result += $"<th><input type=\"text\" class=\"form-control form-control-sm tfilterinput\" data-col=\"{(actions == null ? i : i + 1)}\"></th>";
+                         }
                          result += "</tr>";
                     }
                     result += "</thead>";
                }
 
-               //Формируем тело таблицы
-               if (idBody == null)
-                    result += "<tbody>";
-               else
-                    result += $"<tbody id=\"{idBody}\">";
-               foreach (var item in listItems)
+               return result;
+          }
+
+
+          /// <summary>
+          /// Генерация строк таблицы
+          /// </summary>
+          /// <typeparam name="T"></typeparam>
+          /// <param name="item"></param>
+          /// <param name="replaceStrings"></param>
+          /// <returns></returns>
+          private static string GenerateRow<T>(T item,
+               Dictionary<string, string>? replaceStrings = null)
+          {
+               string result = "";
+               foreach (var propertyInfo in propertyInfos)
                {
-                    result += "<tr>";
-                    foreach (var propertyInfo in propertyInfos)
+                    object? value = propertyInfo.GetValue(item);
+                    //Заменяем значения свойств, значениями из словаря
+                    if (replaceStrings != null)
                     {
-                         object? value = propertyInfo.GetValue(item);
-                         //
-                         if (replaceStrings != null)
+                         foreach (var replItem in replaceStrings)
                          {
-                              foreach (var replItem in replaceStrings)
+                              if (value != null)
                               {
-                                   if (value != null)
+                                   if (replItem.Key == Convert.ToString(value))
                                    {
-                                        if (replItem.Key == Convert.ToString(value))
-                                        {
-                                             value = replItem.Value;
-                                             break;
-                                        }
+                                        value = replItem.Value;
+                                        break;
                                    }
                               }
                          }
-                         result += $@"<td>{value}</td>";
-
                     }
-                    result += "</tr>";
+                    result += $@"<td>{value}</td>";
                }
+               result += "</tr>";
 
-               result += "</tbody></table></div>";
-               return new HtmlString(result);
+               return result;
+          }
+
+          /// <summary>
+          /// Генерация всплывающего меню
+          /// </summary>
+          /// <typeparam name="T"></typeparam>
+          /// <param name="item"></param>
+          /// <param name="actions">Словарь ссылок всплывающего списка</param>
+          /// <returns></returns>
+          private static string GenerateDropDownMenuItem<T>(T item, Dictionary<string, string>? actions = null)
+          {
+               string result = "";
+               if (actions == null)
+                    result += "<tr>";
+               else
+               {
+                    result += $"<td><div class=\"dropdown\">" +
+                         $"<a class=\"btn btn-outline-secondary btn-sm dropdown-toggle\" href=\"#\" role=\"button\"  data-bs-toggle=\"dropdown\">" +
+                         $"<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-sliders\" viewBox=\"0 0 16 16\">\r\n  <path fill-rule=\"evenodd\" d=\"M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z\"/>\r\n</svg>" +
+                         $"</a>" +
+                         $"<ul class=\"dropdown-menu\">";
+                    foreach (var action in actions)
+                    {
+                         string actKey = action.Key;
+                         //Замена значений в строке URL на значение свойства
+                         foreach (var propertyInfo in propertyInfos)
+                         {
+                              string? propValue = propertyInfo.GetValue(item)?.ToString();
+                              string propName = propertyInfo.Name;
+                              if (propValue != null)
+                                   propValue = HttpUtility.UrlEncode(propValue);
+
+                              actKey = actKey.Replace($"*{propName}*", propValue);
+                         }
+                         result += $"<li><a class=\"dropdown-item\" href=\"{actKey}\">{action.Value}</a></li>";
+                    }
+                    result += $"</ul></div>";
+               }
+               return result;
           }
      }
 }
---
+++ MVCHtmlHelpers/wwwroot/js/di.htmlhelpers.table.js
@@ -0,0 +1,46 @@
+﻿//tfilterinput
+const tfiltersinput = document.querySelectorAll('.tfilterinput');
+
+if (tfiltersinput.length > 0) {
+     for (let i = 0; i < tfiltersinput.length; i++) {
+          let col = parseInt(tfiltersinput[i].dataset.col);
+          tfiltersinput[i].addEventListener('input', function () {
+               let value = tfiltersinput[i].value;
+               tableFilter(value, 'tableBody', col);
+          });
+     }
+}
+
+function tableFilter(value, bodyTable, col = -1) {
+     var rows = document.getElementById(bodyTable).rows;         //получаем все строки таблицы
+     for (var i = 0; i < rows.length; i++) {                     //проход по всем строкам в таблице
+          var inpvalue = value.toLowerCase();                 //Перефодим значение в нижний регистр
+          if (inpvalue.length > 0) {                              //если количество символов > 0
+               var cels = rows[i].getElementsByTagName('td');      //получаем все ячейки таблицы
+               var matched = false;                                //переменная для определения совпадения
+               var val;                                            //для получения значения ячейки
+               if (col === -1) {                                   //если проход по всем колонкам таблицы
+                    for (var td = 0; td < cels.length; td++) {      //то проходим по всем ячейкам таблицы
+                         val = cels[td].innerHTML.toLowerCase();     //значение ячейки
+                         if (val.indexOf(inpvalue) > -1) {           //сравниваем со значением инпута
+                              matched = true;                         //совпадение
+                              break;                                  //переходим к следующей строке
+                         }
+                    }
+               } else {                                            //если поиск только в указанной колонке
+                    val = cels[col].innerHTML.toLowerCase();        //значение ячейки указанной колонке в строке
+                    if (val.indexOf(inpvalue) > -1) {               //если найдено совпадение
+                         matched = true;//
+                    }
+               }
+               if (matched === false) { //если после всех проверок не найдено совпадение
+                    rows[i].hidden = true; //скрываем строку
+               } else {
+                    rows[i].hidden = false;
+               }
+          } else {                                                //если симолов для поиска нет, отображаем все строки
+               rows[i].hidden = false;
+          }
+     }
+     return;
+}
